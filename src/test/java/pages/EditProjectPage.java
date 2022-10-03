82a41b14166e396391b57d74b5e1dcc88034971d
NatalyaKhrol <tashapas@yahoo.com>
2022-10-03 22:26:29 +0300

    Some new features added in EditProjectPage

--- src/test/java/pages/EditProjectPage.java
+++ src/test/java/pages/EditProjectPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class EditProjectPage {
     public void editProject() {
@@ -10,5 +11,13 @@ public class EditProjectPage {
         $("[class=\"UdZcu9\"]").click();
         $("[class=\"DJXdnf N7aVnv\"]").click();
         $("#save-case").click();
+        $$("[class=\"nbWgel\"]").get(0).click();
+        $("#create-suite-button").click();
+        $("#name").sendKeys("WebApplication");
+        $("[class=\"ProseMirror toastui-editor-contents\"]").sendKeys("Blabla");
+        $("#save-suite-button").click();
+
+
+
     }
 }
