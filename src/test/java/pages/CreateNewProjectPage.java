78c6d8ddc55177257b21d2415c8c043839994e84
ns-maud96 <ns.maud96@gmail.com>
2022-10-04 19:36:55 +0300

    Correct mistakes

--- src/test/java/pages/CreateNewProjectPage.java
+++ src/test/java/pages/CreateNewProjectPage.java
@@ -1,12 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class CreateNewProjectPage {
+    private By TITLE = By.cssSelector("#inputTitle");
+    private By INPUT_CODE = By.cssSelector("#inputCode");
+
     public void createProject() {
         $("#createButton").click();
-        $("#inputTitle").sendKeys("Qase");
-        $("#inputCode").sendKeys("QS");
+        $(TITLE).sendKeys("Qase");
+        $(INPUT_CODE).sendKeys("QS");
         $("#inputDescription").sendKeys("The third project in Qase");
         $("#public-access-type").click();
         $("[class=\"btn btn-primary\"]").click();
